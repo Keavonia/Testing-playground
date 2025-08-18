a26861de13f363916d7f3b9c98391cbf65a80b5a
yaegi <keavonia@gmail.com>
2025-08-18 13:21:51 +0200

    added: class MessagesAfterAdding

--- bookstore/pages/locators.py
+++ bookstore/pages/locators.py
@@ -11,3 +11,7 @@ class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main > p.price_color")
+
+class MessagesAfterAdding():
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert-success")
+    BASKET_TOTAL_ALERT = (By.CSS_SELECTOR, "div.alert-info")
