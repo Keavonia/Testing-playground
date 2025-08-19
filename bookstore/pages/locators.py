3bee08fcc1c9cb5fbe8e36403942058af621ae0e
yaegi <keavonia@gmail.com>
2025-08-19 12:20:11 +0200

    changed: a more universal locator for user login

--- bookstore/pages/locators.py
+++ bookstore/pages/locators.py
@@ -1,18 +1,23 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main > p.price_color")
 
+
 class MessagesAfterAdding():
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert-success")
     PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert-success div.alertinner > strong")
     BASKET_TOTAL_ALERT = (By.CSS_SELECTOR, "div.alert-info div.alertinner > p strong")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
