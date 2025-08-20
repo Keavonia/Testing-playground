6a19d46cd2ebf5e1e9c60cbaf67778fb34923de7
yaegi <keavonia@gmail.com>
2025-08-20 13:27:35 +0200

    added: an abstract method that verifies that an element does not appear on the page for a specified time.

--- bookstore/pages/base_page.py
+++ bookstore/pages/base_page.py
@@ -1,7 +1,9 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 import math
+from selenium.webdriver.support.wait import WebDriverWait
 from bookstore.pages.locators import BasePageLocators
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage():
@@ -19,6 +21,13 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
 
     def go_to_login_page(self):
         self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID).click()
@@ -39,3 +48,5 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+
