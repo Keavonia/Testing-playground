a5d98b0b3ba558298a07f8bfecaef7b7ac786955
yaegi <keavonia@gmail.com>
2025-08-20 13:31:59 +0200

    added: going to the basket page

--- bookstore/pages/main_page.py
+++ bookstore/pages/main_page.py
@@ -1,5 +1,7 @@
 from .base_page import BasePage
+from .locators import BasePageLocators
 
 class MainPage(BasePage):
 
-    pass
+    def go_to_basket_page(self):
+        self.browser.find_element(*BasePageLocators.BASKET_BUTTON).click()
