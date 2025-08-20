851cb09bb66bdd50775e2b81f6e5bda2481ec4c1
yaegi <keavonia@gmail.com>
2025-08-20 15:24:55 +0200

    added: user registration

--- bookstore/pages/login_page.py
+++ bookstore/pages/login_page.py
@@ -17,3 +17,15 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form not found"
+
+    def  register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        pswd1_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
+        pswd2_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+
+        email_field.send_keys(email)
+        pswd1_field.send_keys(password)
+        pswd2_field.send_keys(password)
+
+        register_button.click()
