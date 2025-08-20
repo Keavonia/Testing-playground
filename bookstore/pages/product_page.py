41be09367d0298090df7d805a39f3730dea0afb3
yaegi <keavonia@gmail.com>
2025-08-20 14:56:01 +0200

    added: "." at the end of error notifications for better readability

--- bookstore/pages/product_page.py
+++ bookstore/pages/product_page.py
@@ -15,14 +15,14 @@ class ProductPage(BasePage):
         product_name_text = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         success_message_text = self.browser.find_element(*MessagesAfterAdding.PRODUCT_NAME_IN_SUCCESS_MESSAGE).text
         assert product_name_text == success_message_text, \
-            f"Product name in message should be '{product_name_text}', but got '{success_message_text}'"
+            f"Product name in message should be '{product_name_text}', but got '{success_message_text}'."
 
     def should_be_product_price_in_success_message(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         basket_total_text = self.browser.find_element(*MessagesAfterAdding.BASKET_TOTAL_ALERT).text
         basket_total_price = basket_total_text.split()[-1]
         assert product_price == basket_total_price, \
-            f"Product price should be '{product_price}', but basket shows '{basket_total_price}'"
+            f"Product price should be '{product_price}', but basket shows '{basket_total_price}'."
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*MessagesAfterAdding.SUCCESS_MESSAGE), \
