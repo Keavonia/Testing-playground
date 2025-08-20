9856ea60d8d74e82c002dee163830f0e7e6d16a9
yaegi <keavonia@gmail.com>
2025-08-20 13:31:18 +0200

    added: new locators for the button to go to the basket page and to check that the basket is empty.

--- bookstore/pages/locators.py
+++ bookstore/pages/locators.py
@@ -21,3 +21,9 @@ class MessagesAfterAdding():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group > a.btn")
+
+
+class BasketPageLocators():
+    BASKET_FORMSET = (By.CSS_SELECTOR, "div.basket-items > div.row")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
