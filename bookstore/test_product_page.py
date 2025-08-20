d86ba1b76521a239104437a9c6cc7b0d99c16129
yaegi <keavonia@gmail.com>
2025-08-20 14:59:28 +0200

    added: new class TestUserAddToBasketFromProductPage for group and setup tests

--- bookstore/test_product_page.py
+++ bookstore/test_product_page.py
@@ -52,3 +52,20 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+class TestUserAddToBasketFromProductPage():
+
+    link = ''
+
+    def test_user_can_add_product_to_basket(browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_be_product_name_in_success_message()
+        page.should_be_product_price_in_success_message()
+
+    def test_user_cant_see_success_message_after_adding_product_to_basket(browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_not_be_success_message()
